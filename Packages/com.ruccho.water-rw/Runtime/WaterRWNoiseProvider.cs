438e3ece2ac07775d6a62a9efad602bbada43571
ruccho <noboroo2000@yahoo.co.jp>
2026-06-18 19:22:13 +0900

    support Unity 6.4 or later

--- Packages/com.ruccho.water-rw/Runtime/WaterRWNoiseProvider.cs
+++ Packages/com.ruccho.water-rw/Runtime/WaterRWNoiseProvider.cs
@@ -7,6 +7,22 @@ namespace Ruccho
         [SerializeField] private float noiseFrequency;
         [SerializeField] private float noiseAmplitude;
 
-        public Vector2 Velocity => new(0f, (Mathf.PerlinNoise1D(noiseFrequency * Time.fixedTime + GetInstanceID() * 0.13f) * 2f - 1f) * noiseAmplitude);
+        public Vector2 Velocity
+        {
+            get
+            {
+#if UNITY_6000_4_OR_NEWER
+                var id = EntityId.ToULong(GetEntityId());
+                Random.InitState(unchecked((int)((uint)(id >> 32) ^ (uint)id)));
+#endif
+                return new Vector2(0f, (Mathf.PerlinNoise1D(noiseFrequency * Time.fixedTime +
+#if UNITY_6000_4_OR_NEWER
+                                                            Random.value * 100f
+#else
+                                                            GetInstanceID()
+#endif
+                                                            * 0.13f) * 2f - 1f) * noiseAmplitude);
+            }
+        }
     }
 }
